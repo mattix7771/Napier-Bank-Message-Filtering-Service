5f6ffc5f49b9bd48baabde162cc7661e54e8d8dd
Mattia Merati <40442647@live.napier.ac.uk>
2022-11-24 16:28:13 +0100

    Added tests

--- FilterTesting/UnitTests.cs
+++ FilterTesting/UnitTests.cs
@@ -5,6 +5,7 @@ using FlaUI.Core;
 using FlaUI.UIA3;
 using FlaUI.Core.Conditions;
 using FlaUI.Core.AutomationElements;
+using System.Linq.Expressions;
 
 namespace FilterTesting
 {
@@ -57,7 +58,7 @@ lbl_MentionsList
         */
 
         [Fact]
-        public void BadInputTest()
+        public void SMSTypeTest()
         {
             //Get wanted elements
             var btn_create = mainWindow.FindFirstDescendant(cf.ByAutomationId("btn_create")).AsButton();
@@ -70,6 +71,74 @@ lbl_MentionsList
             //Check that correct textbox appears
             var txt_messageSMS = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_messageSMS")).AsTextBox();
             Assert.True(txt_messageSMS.IsAvailable);
+        }
+
+        [Fact]
+        public void EmailTypeTest()
+        {
+            //Get wanted elements
+            var btn_create = mainWindow.FindFirstDescendant(cf.ByAutomationId("btn_create")).AsButton();
+            var txt_sender = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_sender")).AsTextBox();
+
+            //Enter text and click button
+            txt_sender.Enter("40442647@live.napier.co.uk");
+            btn_create.Click();
+
+            //Check that correct textbox appears
+            var txt_messageEmail = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_messageEmail")).AsTextBox();
+            Assert.True(txt_messageEmail.IsAvailable);
+        }
+
+        [Fact]
+        public void TweetTypeTest()
+        {
+            //Get wanted elements
+            var btn_create = mainWindow.FindFirstDescendant(cf.ByAutomationId("btn_create")).AsButton();
+            var txt_sender = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_sender")).AsTextBox();
+
+            //Enter text and click button
+            txt_sender.Enter("JohnSmith");
+            btn_create.Click();
+
+            //Check that correct textbox appears
+            var txt_messageTwitter = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_messageTwitter")).AsTextBox();
+            Assert.True(txt_messageTwitter.IsAvailable);
+        }
+
+        [Fact]
+        public void PreviewReadOnlyTest()
+        {
+            //Check that the preview box is readonly
+            var txt_output = mainWindow.FindFirstDescendant(cf.ByAutomationId("txt_output")).AsTextBox();
+            Assert.Throws<System.InvalidOperationException>(() => txt_output.Enter("hello"));
+        }
+        
+        [Fact]
+        public void TabSelectionTest()
+        {
+            //Check that tabs work properly
+            var tab_statistics = mainWindow.FindFirstDescendant(cf.ByName("tab_statistics")).AsTabItem();
+            var lb_TrendList = mainWindow.FindFirstDescendant(cf.ByName("lb_TrendList")).AsListBox();
+            tab_statistics.Click();
+            Assert.True(lb_TrendList.IsAvailable);
+        }
+
+        [Fact]
+        public void Test()
+        {
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+
+        }
+
+        [Fact]
+        public void Test()
+        {
+
         }
     }
 }
